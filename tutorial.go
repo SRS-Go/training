f2e989e5181f093a51693add9328a07de95d31d6
Subhendu Sahu <subhendu@techalphamsg.com>
2022-08-07 19:47:42 +0530

    range on slice and arrays

--- tutorial.go
+++ tutorial.go
@@ -3,18 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	var x [6]int = [6]int{1, 2, 3, 4, 5, 6}
-	var s []int = x[2:5] //where to start slice and where to end (excluding the end)
-	fmt.Println(x)
-	fmt.Println(s, len(s), cap(s)) //length gives the slice lenght, while cap gives the initial array capacity
-	fmt.Println(s[:cap(s)])
-
-	var a []int = []int{11, 22, 33, 44, 55} //Cheat on Go to make array dynamic, initialize with no length
-	fmt.Println(cap(a[:3]))
-
-	a = append(a, 66) //use append method to append element and store it as initial array name
+	var a []int = []int{1, 3, 4, 5, 6, 7, 4, 11}
 	fmt.Println(a)
+	for i, element := range a {
+		fmt.Println(i, element)
+	}
 
-	b := make([]int, 5) //create empty slice
-	fmt.Println(b)
+	// if not using index
+	for _, element := range a {
+		fmt.Println(element)
+	}
+
+	// print only unique element
+	for i, element := range a {
+		for j := i + 1; j < len(a); j++ {
+			if a[j] == element {
+				fmt.Println("duplicates:", element)
+			}
+		}
+	}
+	// print 2d array
+	var arr2d [2][3]int = [2][3]int{{1, 2, 3}, {4, 5, 6}}
+	for _, element := range arr2d {
+		for _, element2 := range element {
+			fmt.Println(element2)
+		}
+	}
 }
