86310372a9192a86e45396f9d537d037151da387
Subhendu Sahu <subhendu@techalphamsg.com>
2022-08-07 18:08:30 +0530

    chained condtion (AND, OR, NOT)

--- tutorial.go
+++ tutorial.go
@@ -1,17 +1,29 @@
 package main
 
 //this is how you import multiple packages
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	//condtional and boolean expressions
-	// operators < > <= >= == !=
+	//chained conditions:
+	// and - &&
+	// or - ||
+	// not !
 	x := 5
 	y := 6
-	val := x < y
+	z := 7
+	a := x == y
+	val := x < y && x < z
 	fmt.Printf("%t", val)
-	val = x == y
+	val = x < y || x < z
+	fmt.Printf("\n%t", val)
+	val = x < y && x > z
+	fmt.Printf("\n%t", val)
+	val = x > y || x > z
+	fmt.Printf("\n%t", val)
+	val = x < y || x > y && x > z //not a good way of writing code
+	fmt.Printf("\n%t", val)
+	val = x < y || (x > y && x > z)
+	fmt.Printf("\n%t", val)
+	val = x < y && !a
 	fmt.Printf("\n%t", val)
 }
