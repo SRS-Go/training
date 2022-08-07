385620e087104e2f800184190feb9c65bde39dc6
Subhendu Sahu <subhendu@techalphamsg.com>
2022-08-07 17:57:20 +0530

    condtional expression and boolean

--- tutorial.go
+++ tutorial.go
@@ -6,15 +6,12 @@ import (
 )
 
 func main() {
-	/*Arithmetic and match operations */
-	var num1 int = 9
-	var num2 int = 4
-	answer := (num1 + num2) % 2
-	fmt.Printf("%d", answer)
-	var num3 float64 = 8
-	var num4 int = 4
-	answer2 := num3 / float64(num4)
-	fmt.Printf("\n%f", answer2)
-	answer3 := num1 % num2
-	fmt.Printf("\n%d", answer3)
+	//condtional and boolean expressions
+	// operators < > <= >= == !=
+	x := 5
+	y := 6
+	val := x < y
+	fmt.Printf("%t", val)
+	val = x == y
+	fmt.Printf("\n%t", val)
 }
