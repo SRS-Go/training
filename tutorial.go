b6009c9b454b7786bb023018132039f0141d8e2e
Subhendu Sahu <subhendu@techalphamsg.com>
2022-08-07 17:44:52 +0530

    arithmetic operations

--- tutorial.go
+++ tutorial.go
@@ -2,17 +2,19 @@ package main
 
 //this is how you import multiple packages
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
 )
 
 func main() {
-	/*Tutorial for input data, change type */
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Printf("Enter the year you were born: ")
-	scanner.Scan()
-	input, _ := strconv.ParseInt(scanner.Text(), 10, 64)
-	fmt.Printf("you will be %d years old at the end of 2022.", 2022-input)
+	/*Arithmetic and match operations */
+	var num1 int = 9
+	var num2 int = 4
+	answer := (num1 + num2) % 2
+	fmt.Printf("%d", answer)
+	var num3 float64 = 8
+	var num4 int = 4
+	answer2 := num3 / float64(num4)
+	fmt.Printf("\n%f", answer2)
+	answer3 := num1 % num2
+	fmt.Printf("\n%d", answer3)
 }
