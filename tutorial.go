e211695c1f68f7dfb742e46fbabd92e46ec43f24
Subhendu Sahu <subhendu@techalphamsg.com>
2022-08-08 18:55:20 +0530

    pointers and de-reference operators

--- tutorial.go
+++ tutorial.go
@@ -2,27 +2,24 @@ package main
 
 import "fmt"
 
-func changeFirst(slice []int) {
-	slice[0] = 1000
+func changeValue(str *string) {
+	*str = "changed!"
+}
+
+func changeValue2(str string) {
+	str = "changed again!"
 }
 
 func main() {
-
-	// slices are mutables (maps as well)
-	var x []int = []int{3, 4, 5}
-	y := x
-	y[0] = 100
+	x := 7
+	y := &x
+	fmt.Println(x, y)
+	*y = 8
 	fmt.Println(x, y)
 
-	// arrays are immutable
-	var xx [2]int = [2]int{1, 2}
-	yy := x
-	yy[1] = 3
-	fmt.Println(xx, yy)
-
-	// change value in other function for mutable data
-	var slice []int = []int{2, 5, 7}
-	fmt.Println(slice)
-	changeFirst(slice)
-	fmt.Println(slice)
+	toChange := "hello"
+	changeValue(&toChange) // pass pointer if you want to change value
+	fmt.Println(toChange)
+	changeValue2(toChange)
+	fmt.Println(toChange)
 }
