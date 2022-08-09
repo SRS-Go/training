55d1775f564a0710d7118ac39558e0a33a9aabc7
Subhendu Sahu <subhendu@techalphamsg.com>
2022-08-09 10:07:47 +0530

    how to create methods

--- tutorial.go
+++ tutorial.go
@@ -3,40 +3,44 @@ package main
 import "fmt"
 
 //this is how you define struct
-type Point struct {
-	x int32
-	y int32
-	z bool
+type Student struct {
+	name   string
+	grades []int
+	age    int
 }
 
-func changeX(pt *Point) {
-	pt.x = 100
+func (s Student) getAge() int {
+	return s.age
 }
 
-type Circle struct {
-	radius float64
-	center *Point
+func (s *Student) setAge(age int) {
+	s.age = age
 }
 
-type AnotherCircle struct {
-	radius float64
-	*Point // no need to give a name.. as long as there are no common attributes on both structs it will work
+func (s Student) getAverageGrade() float32 {
+	sum := 0
+	for _, v := range s.grades {
+		sum += v
+	}
+	return float32(sum) / float32(len(s.grades))
+}
+
+// this how to define methods for structs
+func (s *Student) getMaxGrade() int {
+	curMax := 0
+	for _, v := range s.grades {
+		if curMax < v {
+			curMax = v
+		}
+	}
+	return curMax
 }
 
 func main() {
-	var p1 Point = Point{1, 2, false}
-	fmt.Println(p1.x, p1.y)
-	var p2 Point = Point{x: 2} // passing one value, defaults other
-	fmt.Println(p2.x, p2.y)
-	p3 := &Point{y: 3} // assign as pointer instead of object, benefits if you want to change values in other functions
-	changeX(p3)
-	fmt.Println(p3.x)
-	// struct inside a struct
-	p4 := &Point{x: 7}
-	c1 := &Circle{3.75, p4}
-	fmt.Println(c1)
-	c2 := &Circle{3.75, &Point{4, 5, true}}
-	fmt.Println(c2, c2.center, c2.center.y)
-	c3 := &AnotherCircle{3.4, &Point{x: 4}}
-	fmt.Println(c3.radius, c3.x, c3.y, c3.z) // access the inner struct attribute directly
+	s1 := Student{"John", []int{65, 55, 59}, 19}
+	fmt.Println(s1.age)
+	s1.setAge(20)
+	fmt.Println(s1.getAge())
+	fmt.Println(s1.getAverageGrade())
+	fmt.Println(s1.getMaxGrade())
 }
